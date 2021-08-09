622112ab5c45d1e3014f95c061287cd7881e4907
davidhaffner <david.haffner06@gmail.com>
2021-08-10 00:11:24 +0200

    fonctionnel mais sans style

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,39 +1,31 @@
+require 'open-uri'
+
 class GamesController < ApplicationController
+
   def start
     def generate_grid(grid_size)
-      # TODO: generate random grid of letters
       Array.new(grid_size) { ('A'..'Z').to_a.sample }
     end
     @new_grid = generate_grid(9)
     @start_time = Time.now
-
-  
-    
-    
-    
   end
 
   def result
-    @end_time = Time.now
+
+    def english_word?(word)
+      response = URI.open("https://wagon-dictionary.herokuapp.com/#{word}")
+      json = JSON.parse(response.read)
+      return json['found']
+    end
 
     def included?(guess, grid)
       guess.chars.all? { |letter| guess.count(letter) <= grid.count(letter) }
     end
-    
+
     def compute_score(attempt, time_taken)
       time_taken > 60.0 ? 0 : attempt.size * (1.0 - time_taken / 60.0)
     end
-    
-    def run_game(attempt, grid, start_time, end_time)
-      # TODO: runs the game and return detailed hash of result (with `:score`, `:message` and `:time` keys)
-      result = { time: end_time - start_time }
-    
-      score_and_message = score_and_message(attempt, grid, result[:time])
-      result[:score] = score_and_message.first
-      result[:message] = score_and_message.last
-    
-      result
-    end
+
     def score_and_message(attempt, grid, time)
       if included?(attempt.upcase, grid)
         if english_word?(attempt)
@@ -46,11 +38,22 @@ class GamesController < ApplicationController
         [0, "not in the grid"]
       end
     end
-    
-    def english_word?(word)
-      response = URI.open("https://wagon-dictionary.herokuapp.com/#{word}")
-      json = JSON.parse(response.read)
-      return json['found']
+
+    def run_game(attempt, grid, start_time, end_time)
+      result = { time: end_time - start_time }
+      score_and_message = score_and_message(attempt, grid, result[:time])
+      result[:score] = score_and_message.first
+      result[:message] = score_and_message.last
+
+      result
     end
+
+    # @start_time = Timestamp.valueOf(params[:start_time])
+    @start_time = Time.now
+    sleep(2)
+    @end_time = Time.now
+    @attempt = params[:result]
+    @grid = params[:new_grid]
+    @score = run_game(@attempt, @grid, @start_time, @end_time)
   end
 end
